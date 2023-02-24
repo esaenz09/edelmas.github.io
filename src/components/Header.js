7cc0a63e285dbc2eafd90b26bca8655f209722a0
edelma <edelma8@gmail.com>
2023-02-23 22:06:07 -0500

    fixed mobile nav collapse/highlights active state

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,39 +5,31 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className='my-4 mx-5'>
-      <Navbar expand='md'>
+      <Navbar collapseOnSelect expand='md'>
         <Navbar.Brand>
-          <Link className="fs-2 text-decoration-none text-black" to="/">
+          <Nav.Link eventKey="1" as={Link} to="/">
             <img
               width="55"
               height="55"
               src="ESIcon.png"
               alt="website logo"
             />
-          </Link>
+          </Nav.Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse>
-          <Nav className='ms-auto'>
-            <Nav.Link>
-              <Link className="fs-5 text-decoration-none text-secondary" to="/about">
-                About
-              </Link>
+          <Nav className='fs-5 ms-auto'>
+            <Nav.Link eventKey="2" as={Link} to="/about">
+              About
             </Nav.Link>
-            <Nav.Link>
-              <Link className="fs-5 text-decoration-none text-secondary" to="/experience">
-                Experience
-              </Link>
+            <Nav.Link eventKey="3" as={Link} to="/experience">
+              Experience
             </Nav.Link>
-            <Nav.Link>
-              <Link className="fs-5 text-decoration-none text-secondary" to="/resume">
-                Resume
-              </Link>
+            <Nav.Link eventKey="4" as={Link} to="/resume">
+              Resume
             </Nav.Link>
-            <Nav.Link>
-              <Link className="fs-5 text-decoration-none text-secondary" to="/contact">
-                Contact
-              </Link>
+            <Nav.Link eventKey="5" as={Link} to="/contact">
+              Contact
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
